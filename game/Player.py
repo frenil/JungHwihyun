a5ea1a3ec24a86a491f282b485ae1eaeba83bf06
jung <silsen@naver.com>
2016-10-11 23:20:30 +0900

    update

--- game/Player.py
+++ game/Player.py
@@ -2,39 +2,31 @@ from pico2d import *
 
 class Ragna:
     def __init__(self):
-        self.x,self.y = 900,450
+        self.x,self.y = 900,150
         self.frame =0
         self.state =0
         self.st2co=0
         self.stcount =0
         self.see = 0
         self.walkframe= 0
-        self.Lwalk = load_image('Lwalk.png')
-        self.Rwalk = load_image('Rwalk.png')
-        self.Lstand = load_image('stand1.png')
-        self.Rstand = load_image('Rstand1.png')
-        self.stand2 = load_image('stand2.png')
-        self.Lpunch = load_image('punch.png')
-        self.Rpunch = load_image('Rpunch.png')
-        self.nomco1 = load_image('nomco1.png')
-        self.Rnomco1 = load_image('Rnomco1.png')
-        self.nomco2 = load_image('nomco2.png')
-        self.Rnomco2 = load_image('Rnomco2.png')
-        self.nomco3 = load_image('nomco3.png')
+        self.Lwalk = load_image('resource/Lwalk.png')
+        self.Rwalk = load_image('resource/Rwalk.png')
+        self.Lstand = load_image('resource/stand1.png')
+        self.Rstand = load_image('resource/Rstand1.png')
+        self.stand2 = load_image('resource/stand2.png')
+        self.Lpunch = load_image('resource/punch.png')
+        self.Rpunch = load_image('resource/Rpunch.png')
+        self.nomco1 = load_image('resource/nomco1.png')
+        self.Rnomco1 = load_image('resource/Rnomco1.png')
+        self.nomco2 = load_image('resource/nomco2.png')
+        self.Rnomco2 = load_image('resource/Rnomco2.png')
+        self.nomco3 = load_image('resource/nomco3.png')
+        self.Rnomco3 = load_image('resource/Rnomco3.png')
+
     def update(self):
         if self.state ==0 :
             self.frame = (self.frame+1)%13
             self.stcount+=1
-            if self.stcount == 52:
-                self.state = 1
-                self.stcount =0
-                self.frame =0
-        elif self.state == 1:
-            self.frame = (self.frame+1)%17
-            self.st2co +=1
-            if self.st2co>16:
-                self.state = 0
-                self.st2co = 0
         elif self.state == 2:
             self.frame = self.frame+1
         elif self.state == 211:
@@ -55,8 +47,6 @@ class Ragna:
                     self.Lstand.clip_draw_to_origin(self.frame * 350, 0, 350, 450, self.x, self.y, 175, 225)
                 else:
                     self.Lstand.clip_draw_to_origin((13 - self.frame) * 350, 0, 350, 450, self.x, self.y, 175, 225)
-            elif self.state == 1:
-                self.stand2.clip_draw_to_origin(self.frame * 350, 0, 350, 350, self.x, self.y, 175, 225)
             elif self.state == 2:
                 self.Lpunch.clip_draw_to_origin(self.frame * 480, 0, 480, 430, self.x - 90, self.y, 240, 215)
             elif self.state == 211:
@@ -102,12 +92,12 @@ class Ragna:
                     self.Rnomco2.clip_draw_to_origin(self.frame * 800, 430, 800, 430, self.x , self.y, 400, 215)
             elif self.state == 213:
                 if self.frame < 7:
-                    self.nomco3.clip_draw_to_origin(self.frame * 1100, 1000, 1100, 490, self.x - 300, self.y, 550, 250)
+                    self.Rnomco3.clip_draw_to_origin(self.frame * 1100, 1000, 1100, 490, self.x -90, self.y, 550, 250)
                 elif self.frame < 14:
-                    self.nomco3.clip_draw_to_origin((self.frame - 7) * 1100, 500, 1100, 490, self.x - 330, self.y, 550,
+                    self.Rnomco3.clip_draw_to_origin((self.frame - 7) * 1100, 500, 1100, 490, self.x -90, self.y, 550,
                                                     250)
                 else:
-                    self.nomco3.clip_draw_to_origin((self.frame - 14) * 1100, 0, 1100, 490, self.x - 330, self.y, 550,
+                    self.Rnomco3.clip_draw_to_origin((self.frame - 14) * 1100, 0, 1100, 490, self.x -90, self.y, 550,
                                                     250)
             elif self.state == 3:
                 self.Rwalk.clip_draw_to_origin(self.frame * 300, 0, 300, 450, self.x, self.y, 150, 225)
---
+++ game/main.py
@@ -0,0 +1,120 @@
+from pico2d import *
+import Player
+
+def handle_events():
+    global running
+    global next
+    global Ldown, Rdown
+    global see
+    global Ldouble,Rdouble
+    global LKeyco, RKeyco
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+            elif event.key == SDLK_a :
+                next = 1
+            if event.key == SDLK_LEFT:
+                Ldown = True
+                see = -1
+                if LKeyco < 5:
+                    Ldouble = True
+            if event.key == SDLK_RIGHT:
+                Rdown = True
+                see = 1
+                if RKeyco < 5:
+                    Rdouble = True
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_LEFT:
+                Ldown = False
+                if Rdown == True:
+                    see = 1
+                if Ldouble == True:
+                    LKeyco =0
+            if event.key== SDLK_RIGHT:
+                Rdown = False
+                if Ldown == True:
+                    see = -1
+                if Rdouble == True:
+                    RKeyco =0
+
+open_canvas(1800,800)
+
+player = Player.Ragna()
+state =0
+running = True
+stcount = 0
+next = 0
+frame=0
+see =1
+LKeyco , RKeyco = 0 , 0
+Ldouble, Rdouble = False, False
+Ldown, Rdown = False, False
+walking =False
+while running:
+    handle_events()
+    player.see = see
+    if Ldown == False and Rdown == False:
+        walking = False
+    if player.state == 0 :
+        if Ldown == True or Rdown == True:
+            walking = True
+        if next==0 and walking == True:
+            player.state = 3
+            player.see = see
+        elif next == 1:
+            player.state = 2
+            next = 0
+            player.frame = 0
+            frame = 0
+    elif player.state == 3 :
+        if walking == False:
+            player.walkframe =0
+            player.state =0
+            player.frame=0
+            frame =0
+        elif frame<5 and (Rdouble== True or Ldouble==True):
+            pass
+    elif player.state ==2 and frame==5:
+        if next == 0:
+            player.state = 0
+        elif next == 1 :
+            player.state = 211
+        next = 0
+        player.frame = 0
+        frame=0
+    elif player.state ==211and frame == 17:
+        if next == 0:
+            player.state = 0
+        elif next == 1 :
+            player.state = 212
+        player.frame = 0
+        next = 0
+        frame=0
+    elif player.state ==212 and frame ==16:
+        if next == 0:
+            player.state = 0
+        elif next == 1:
+            player.state = 213
+        player.frame = 0
+        frame=0
+        next=0
+    elif  player.state == 213and frame ==20:
+        player.state=0
+        player.frame = 0
+        frame=0
+        next=0
+    player.update()
+    frame +=1
+    clear_canvas()
+
+    player.draw()
+
+    update_canvas()
+    delay(0.04)
+
+close_canvas()
+
