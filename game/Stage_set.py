1bbb6456c3c2e556c80d1bb8a0ba282e00fc5fc7
frenil <silsen@naver.com>
2016-11-15 22:25:49 +0900

    update

---
+++ game/Stage_set.py
@@ -0,0 +1,31 @@
+def Speed(kmph):
+    PIXEL_PER_METER = (100.0 / 0.8)  # 100픽셀 0.8m
+    SPEED_KMPH = kmph
+    SPEED_MPM = (SPEED_KMPH * 1000 / 60)
+    SPEED_MPS = SPEED_MPM / 60
+    SPEED_PPS = SPEED_MPS * PIXEL_PER_METER
+    distance = SPEED_PPS * game_framework.frame_time
+    return distance
+
+class stage:
+    def __init__(self):
+        self.Dallnumber = 0
+        self.dallx = [0,0,0,0,0,0,0,0,0,0]
+        self.dall_HP = 1000
+        self.speed = [0,0,0,0,0,0,0,0,0,0]
+        self.Playerx = 300
+    def update(self,number):
+        if number== 1:
+            self.Dallnumber=10
+            self.Playerx = 300
+            self.dall_HP = 1000
+            for i in range(self.Dallnumber):
+                self.dallx[i]= -300+(250*i)
+                self.speed[i] = (i%4)+1
+        elif number ==2:
+            self.Dallnumber = 5
+            self.Playerx = 60
+            self.dall_HP = 3000
+            for i in range(self.Dallnumber):
+                self.dallx[i] = 400+(100 * i)
+                self.speed[i] = (i % 5) + 4
