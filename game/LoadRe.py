79d16c9123be93745afaed4e9b25c82bfe7c376f
frenil <silsen@naver.com>
2016-11-23 16:46:03 +0900

    update

--- game/LoadRe.py
+++ game/LoadRe.py
@@ -7,32 +7,54 @@ def Loading():
     global rag
     global dall
     global font
+    font = load_font('ENCR10B.TTF', 100)
+    image = load_image('title.png')
     rag = Ragna_image()
     dall = Dall_image()
-    rag.Image_load()
+    rag.Image_load(image,font)
 class Dall_image:
     def __init__(self):
+        pass
+    def Image_load(self,image,font):
+        global LoadCount
+        clear_canvas()
         self.Lstand = load_image('resource/dallstand.png')
         self.Rstand = load_image('resource/Rdallstand.png')
+        LoadCount +=1
+        image.draw_now(640, 360)
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        update_canvas()
+
+        clear_canvas()
         self.Lhit = load_image('resource/dallhit.png')
         self.Rhit = load_image('resource/Rdallhit.png')
+        LoadCount +=1
+        image.draw_now(640, 360)
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        update_canvas()
+
+        clear_canvas()
         self.Lpunch = load_image('resource/dallPunch.png')
         self.Rpunch = load_image('resource/RdallPunch.png')
 
 
+        LoadCount += 1
+        image.draw_now(640, 360)
+        font.draw(300, 100, "Loading  %d" % LoadCount, (225, 225, 225))
+        update_canvas()
+
+
 class Ragna_image:
     def __init__(self):
-        self.font = load_font('ENCR10B.TTF',100)
-
-    def Image_load(self):
+        pass
+    def Image_load(self,image,font):
         global LoadCount
 
         clear_canvas()
-        image = load_image('title.png')
         self.Lwalk = load_image('resource/Lwalk.png')
         self.Rwalk = load_image('resource/Rwalk.png')
         image.draw_now(640, 360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -40,7 +62,7 @@ class Ragna_image:
         self.Lstand = load_image('resource/stand1.png')
         self.Rstand = load_image('resource/Rstand1.png')
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -48,7 +70,7 @@ class Ragna_image:
         self.stand2 = load_image('resource/stand2.png')
         self.Lpunch = load_image('resource/punch.png')
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -56,7 +78,7 @@ class Ragna_image:
         self.Rpunch = load_image('resource/Rpunch.png')
         self.nomco1 = load_image('resource/nomco1.png')
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -64,7 +86,7 @@ class Ragna_image:
         self.Rnomco1 = load_image('resource/Rnomco1.png')
         self.nomco2 = load_image('resource/nomco2.png')
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -72,7 +94,7 @@ class Ragna_image:
         self.Rnomco2 = load_image('resource/Rnomco2.png')
         self.nomco3 = load_image('resource/nomco3.png')
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -80,7 +102,7 @@ class Ragna_image:
         self.Rnomco3 = load_image('resource/Rnomco3.png')
         self.dash = load_image('resource/dash1.png')
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -88,7 +110,7 @@ class Ragna_image:
         self.Rdash = load_image('resource/Rdash1.png')
         self.dash2 = load_image('resource/dash2.png')
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -96,7 +118,7 @@ class Ragna_image:
         self.Rdash2 = load_image('resource/Rdash2.png')
         self.jump_up = load_image('resource/jump_up.png')
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         LoadCount +=1
         update_canvas()
 
@@ -105,7 +127,7 @@ class Ragna_image:
         self.jump_down = load_image('resource/jump_down.png')
         LoadCount +=1
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         update_canvas()
 
         clear_canvas()
@@ -113,7 +135,7 @@ class Ragna_image:
         self.upcom1 = load_image('resource/upcom1.png')
         LoadCount +=1
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         update_canvas()
 
         clear_canvas()
@@ -121,7 +143,7 @@ class Ragna_image:
         self.upcom2 = load_image('resource/upcom2.png')
         LoadCount +=1
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         update_canvas()
 
         clear_canvas()
@@ -129,7 +151,7 @@ class Ragna_image:
         self.upcom3 = load_image('resource/upcom3.png')
         LoadCount +=1
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         update_canvas()
 
         clear_canvas()
@@ -138,7 +160,7 @@ class Ragna_image:
         self.Rupcom4 = load_image('resource/Rupcom4.png')
         LoadCount +=1
         image.draw_now(640,360)
-        self.font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
+        font.draw(300,100,"Loading  %d"%LoadCount,(225,225,225))
         update_canvas()
 
         self.hit = load_image('resource/hit.png')
